d1e6accfcbe12a34aa3e097524ec3c5fb6a6725e
James Gambrill <jameswgambrill@icloud.com>
2024-10-22 14:34:40 +0100

    book complete

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,16 @@
 // create your Media class:
+class Media{
+    
+    static totalMediaCount = 0//how many products have been created
+    constructor(title, year, genre){
+        this.title = title
+        this.year = year
+        this.genre = genre
+    }
 
+    summary(){
+        return `Title: ${title}, Year: ${year}, Genre: ${genre}`
+    }
+}
 // uncomment below to export it:
 module.exports = Media;
