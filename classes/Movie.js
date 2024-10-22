040c93f4db0db7aad230d9acbdad6ccecd8e3acc
James Gambrill <jameswgambrill@icloud.com>
2024-10-22 17:24:45 +0100

    FINISHED: Errors to do with :placement and logic fixed - silly mistakes

--- classes/Movie.js
+++ classes/Movie.js
@@ -14,20 +14,20 @@ class Movie extends Media {
     }
 
     summary(){
-        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.duration}, Rating: ${this.rating}, Director: ${this.director}`
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
     }
 
     static longestMovie(movies) {
         if (movies.length === 0) {
             return null; // Return null if the array is empty
         }
-        let longestmov = movies[0]; // Assume the first movie has the longest playtime
-        for (let i = 1; i < movies.duration; i++) {
-            if (movies[i].duration > longestmov.duration) {
-                longestmov = movies[i]; // Update if a higher rating is found
+        let longestMovie = movies[0]; // Assume the first movie has the longest playtime
+        for (let i = 1; i < movies.length; i++) {
+            if (movies[i].duration > longestMovie.duration) {
+                longestMovie = movies[i]; // Update if a higher rating is found
             }
         }
-        return longestmov;
+        return longestMovie;
     }
 
 }
--- classes/Music.js
+++ classes/Music.js
@@ -9,8 +9,10 @@ class Music extends Media{
         this.artist = artist
         this.length = length
         
-        
-       
+    }
+
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
     }
 
     static shortestAlbum(songs) { //returns shortest song length
