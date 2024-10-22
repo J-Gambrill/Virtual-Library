7641a27f7f2559955dd4013a236e230c329d2adc
James Gambrill <jameswgambrill@icloud.com>
2024-10-22 15:26:22 +0100

    all classes complete

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,34 @@
 // import the Media class:
-import =
-// create your Movie class:
+const { Media } = require("./classes/media.js")
 
+// create your Movie class:
+class Movie extends Media {
+    constructor(director, rating, duration, genre, title, year){
+        this.director = director 
+        this.rating = rating
+        super(duration)
+        super(genre)
+        super(title)
+        super(year)
+    }
+
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.duration}, Rating: ${this.rating}, Director: ${this.director}`
+    }
+
+    static longestMovie(movies) {
+        if (movies.length === 0) {
+            return null; // Return null if the array is empty
+        }
+        let longestmov = movies[0]; // Assume the first movie has the longest playtime
+        for (let i = 1; i < movies.duration; i++) {
+            if (movies[i].duration > longestmov.duration) {
+                longestmov = movies[i]; // Update if a higher rating is found
+            }
+        }
+        return longestmov;
+    }
+
+}
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,27 @@
 // import the Media class:
-
+const { Media } = require("./classes/media.js")
 // create your Music class:
+class Music extends Media{
+    constructor(artist, length, title, year, genre){
+        this.artist = artist
+        this.length = length
+        super(title)
+        super(year)
+        super(genre)
+    }
 
+    static shortestAlbum(songs) { //returns shortest song length
+        if (songs.length === 0) {
+            return null; // Return null if the array is empty
+        }
+        let shortestalbumlength = songs[0]; // Assume the first song has the least time
+        for (let i = 1; i < songs.length; i++) {
+            if (songs[i].length < shortestalbumlength.length) {
+                shortestalbumlength= songs[i]; // Update if a lower length is found
+            }
+        }
+        return shortestalbumlength;
+    }
+}
 // don't change below
 module.exports = Music;
